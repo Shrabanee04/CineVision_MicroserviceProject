677b52609462606f44ec4a88c80cd139fa6c9a9f
Shrabanee Routray <shrabaneeroutray@gmail.com>
2026-06-23 00:23:11 +0530

    Add method to retrieve unnotified events by date range
    Added method to find events that are not notified and within a specific date range.

--- src/main/java/com/example/lifeloopbackend/repository/EventRepository.java
+++ src/main/java/com/example/lifeloopbackend/repository/EventRepository.java
@@ -2,8 +2,15 @@ package com.example.lifeloopbackend.repository;
 
 import com.example.lifeloopbackend.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface EventRepository
         extends JpaRepository<Event, Long> {
+
+    List<Event> findByNotifiedFalseAndEventDateTimeBetween(
+            LocalDateTime start,
+            LocalDateTime end
+    );
 
 }
